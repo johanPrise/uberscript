77866c7e7f7f329cd392e9c3f3f23cfa6b7d0997
Johan Priso <prisojohan2@gmail.com>
2026-03-13 17:23:34 +0100

    feat: mise à jour de la fonction displayMeals pour afficher les repas récupérés et modification de la configuration TypeScript pour cibler ES2022

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,34 @@
-// src/app.ts
 import { fetchMeals } from "./api/meals.api.js";
+import { Meal } from "./models/meals.js";
 
-async function testerAPI() {
-    console.log("Appel de l'API en cours...");
-    const repas = await fetchMeals();
-    console.log("Résultat reçu de l'API :", repas);
+const mealListElement = document.getElementById("mealList") as HTMLUListElement;
+
+
+
+const displayMeals = (meals: Meal[]) => {
+    if (!mealListElement){
+        console.error("Élement 'mealList' introuvable dans le DOM.");
+    }
+    mealListElement.innerHTML = "";
+
+    for(const meal of meals){
+        const li = document.createElement("li");
+        li.className = "list-group-item d-flex justify-content-between align-items-center";
+
+        li.innerHTML = `
+            <div>
+                <strong>${meal.name}</strong> 
+                <span class="badge bg-secondary ms-2">${meal.price}€</span>
+                <br>
+                <small class="text-muted">${meal.calories} kcal</small>
+            </div>
+            <button class="btn btn-primary btn-sm" data-id="${meal.id}">Commander</button>
+        `;
+
+        mealListElement.appendChild(li);
+
+    }   
 }
 
-testerAPI();
+    const meals = await fetchMeals();
+    displayMeals(meals);
